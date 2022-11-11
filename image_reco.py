8e2c37ad5c7f59110e3d9daf00fd8ed77e9bcfa4
kazuki-matsumoto <gato.kazus@icloud.com>
2022-11-11 13:24:42 +0900

    commit

--- image_reco.py
+++ image_reco.py
@@ -1,12 +1,14 @@
+from matplotlib.pyplot import imshow
 import cv2
 from datetime import datetime
+import os
 
 # /dev/video0を指定
 DEV_ID = 0
 
 # パラメータ
-WIDTH = 640
-HEIGHT = 480
+WIDTH = 330
+HEIGHT = 220
 
 def main():
     # /dev/video0を指定
@@ -22,7 +24,24 @@ def main():
         # ファイル名に日付を指定
         date = datetime.now().strftime("%Y%m%d_%H%M%S")
         path = "./pictures/" + date + ".jpg"
+        
+        cascade_path = os.path.join(
+            cv2.data.haarcascades, "haarcascade_frontalface_alt.xml"
+        )
+        
+        face_cascade = cv2.CascadeClassifier(cascade_path)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        #顔を検出
+        faces = face_cascade.detectMultiScale(gray)
+        
+        for x, y, w, h in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=2)
+        
         cv2.imwrite(path, frame)
+
+        
 
     # 後片付け
     cap.release()
