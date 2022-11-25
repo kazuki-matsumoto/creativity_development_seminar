d421d69ba7b07daf54c1dfa4f799319988056c4d
sd20042 <sd20042@tti-j.net>
2022-11-25 15:38:37 +0900

    add_link

--- tset.py
+++ tset.py
@@ -43,3 +43,5 @@ fn_voice_recog()
 
 
 # https://software-data-mining.com/python%E3%81%A8%E9%9F%B3%E5%A3%B0%E8%AA%8D%E8%AD%98%E3%83%95%E3%83%AA%E3%83%BC%E3%82%BD%E3%83%95%E3%83%88%E3%82%A6%E3%82%A7%E3%82%A2julius%E3%81%AE%E9%80%A3%E6%90%BA%E3%81%AB%E3%82%88%E3%82%8B/
+
+# https://www.matematetea.com/entry/2021/12/02/Python%E3%81%A7Julius%E3%82%92%E5%8B%95%E3%81%8B%E3%81%97%E3%81%A6%E9%9F%B3%E5%A3%B0%E8%AA%8D%E8%AD%98%E3%82%B7%E3%82%B9%E3%83%86%E3%83%A0%E4%BD%9C%E3%81%A3%E3%81%A6%E3%81%BF%E3%81%9F%EF%BC%81
