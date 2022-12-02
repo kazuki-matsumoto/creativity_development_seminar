9a6931c2e46066a6ac96ccf34f1159a6a50e2e38
sd20042 <sd20042@tti-j.net>
2022-12-02 17:47:43 +0900

    hobodekita

--- subprocess_test.py
+++ subprocess_test.py
@@ -1,12 +1,13 @@
 import subprocess
-from subprocess import PIPE
- 
-julius      = "dictation-kit-v4.3.1/julius"
-main        = "dictation-kit-v4.3.1/main.jconf"
-am_gmm      = "dictation-kit-v4.3.1/am-dnn.jconf" 
- 
- 
-args = [julius, "-C", main, "-C", am_gmm, "-demo", "-charconv", "utf-8", "sjis"]
- 
-p = subprocess.run(args, stdout=PIPE, text=True)
-print(p.stdout)
+
+def julius_subprocess():
+  julius      = "dictation-kit-v4.3.1-linux/julius"
+  main        = "dictation-kit-v4.3.1-linux/main.jconf"
+  am_gmm      = "dictation-kit-v4.3.1-linux/am-dnn.jconf" 
+  
+  
+  args = [julius, "-C", main, "-C", am_gmm, "-module", "charconv", "utf-8", "sjis"]
+  
+  p = subprocess.run(args)
+
+julius_subprocess()
